649db7c5b688e5fce016611d2787727c6e9b288b
Finn Fallowfield <finnfallowfield@gmail.com>
2015-03-11 08:08:46 +0000

    Add player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 6f;
+
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRb;
+	int floorMask;
+
+	// Use this for initialization
+	void Awake () {
+		playerRb = GetComponent<Rigidbody>();
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+	}
+
+	void Update () {
+
+	}
+
+	void Rotation () {
+		
+
+	}
+}
